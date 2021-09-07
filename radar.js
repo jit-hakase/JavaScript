8e56b87c3d207aece072903d96408f0fdb0cc4a0
hakase <cb634780751@qq.com>
2021-09-07 09:58:41 +0800

    Create radar.js

---
+++ radar.js
@@ -0,0 +1,30 @@
+let chart = {}
+let option = {
+    title: {
+        text: 'Ability'
+    },
+    radar: {
+        indicator: [
+            { name: 'Linux', max: 100 },
+            { name: 'C++', max: 100 },
+            { name: 'JavaScript', max: 100 },
+            { name: 'Java', max: 100 },
+            { name: 'SQL', max: 100 },
+            { name: 'Python', max: 100 }
+        ]
+    },
+    series: [{
+        type: 'radar',
+        data: [
+            {
+                value: [100, 100, 100, 100, 100, 100],
+            },
+            {
+                areaStyle: {},
+                value: [78, 92, 68, 73, 64, 76],
+            }
+        ]
+    }]
+}
+
+chart.setOption(option)
